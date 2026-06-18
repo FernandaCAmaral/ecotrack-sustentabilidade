99d084d7a68ccdae58e75373e4885a188b8f99ed
Fernanda Amaral <camaralfernanda@gmail.com>
2026-06-18 15:36:59 -0300

    feat(list): adiciona suporte a listas de iniciativas e menu interativo via console

--- src/com/github/fernandacamaral/ecotrack/Main.java
+++ src/com/github/fernandacamaral/ecotrack/Main.java
@@ -4,15 +4,75 @@ import com.github.fernandacamaral.ecotrack.modelos.EnergiaRenovavel;
 import com.github.fernandacamaral.ecotrack.modelos.Reflorestamento;
 import com.github.fernandacamaral.ecotrack.servicos.CalculadoraDeImpacto;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Reflorestamento i1 = new Reflorestamento("Bosque", 5000, 150);
-        EnergiaRenovavel i2 = new EnergiaRenovavel("Painés Fábrica", 20000, 12.5);
 
+        Scanner scanner = new Scanner(System.in);
         CalculadoraDeImpacto calculadora = new CalculadoraDeImpacto();
-        calculadora.registra(i1);
-        calculadora.registra(i2);
 
-        System.out.println(calculadora.getTotalDeCreditos());
+        int entrada = 0;
+        while (entrada != 4) {
+            System.out.println("\n______ ECOTRACK - MENU PRINCIPAL ______");
+            System.out.println("""
+                    1 - Cadastrar Projeto de Reflorestamento
+                    2 - Cadastrar Projeto de Energia Renovável
+                    3 - Emitir Relatório de Impacto Ambiental
+                    4 - Sair""");
+            System.out.print("Escolha uma opção: ");
+
+            entrada = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (entrada) {
+                case 1:
+                    System.out.print("Digite o nome da Iniciativa: ");
+                    String nomeReflorestar = scanner.nextLine();
+
+                    System.out.print("Agora o custo da implementação: R$ ");
+                    double custoReflorestar = scanner.nextDouble();
+
+                    System.out.print("E quantidades de árvores plantadas: ");
+                    int arvoresPlantadas = scanner.nextInt();
+                    scanner.nextLine();
+
+                    Reflorestamento novoReflorestamento = new Reflorestamento(nomeReflorestar, custoReflorestar, arvoresPlantadas);
+
+                    calculadora.registra(novoReflorestamento);
+                    System.out.println("✅ Projeto de Reflorestamento registrado com sucesso!");
+                    break;
+
+                case 2:
+                    System.out.print("Digite o nome da Iniciativa: ");
+                    String nomeEnergia = scanner.nextLine();
+
+                    System.out.print("Agora o custo de implementação: R$ ");
+                    double custoEnergia = scanner.nextDouble();
+
+                    System.out.print("E a quantidade de Megawatts gerados: ");
+                    double megawatts = scanner.nextDouble();
+                    scanner.nextLine();
+
+                    EnergiaRenovavel novaEnergia = new EnergiaRenovavel(nomeEnergia, custoEnergia, megawatts);
+
+                    calculadora.registra(novaEnergia);
+                    System.out.println("✅ Projeto de Energia Renovável registrado com sucesso!");
+                    break;
+
+                case 3:
+                    calculadora.geraRelatorioGeral();
+                    break;
+
+                case 4:
+                    System.out.println("Encerrando o EcoTrack... ");
+                    break;
+
+                default:
+                    System.out.println("❌ Opção inválida! Tente novamente.");
+            }
+        }
+        scanner.close();
     }
 }
--- src/com/github/fernandacamaral/ecotrack/servicos/CalculadoraDeImpacto.java
+++ src/com/github/fernandacamaral/ecotrack/servicos/CalculadoraDeImpacto.java
@@ -2,19 +2,36 @@ package com.github.fernandacamaral.ecotrack.servicos;
 
 import com.github.fernandacamaral.ecotrack.modelos.IniciativaSustentavel;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalculadoraDeImpacto {
-    private double totalDeCreditos = 0.0;
+    private List<IniciativaSustentavel> iniciativas = new ArrayList<>();
 
     public void registra(IniciativaSustentavel iniciativa) {
-        iniciativa.exibeDetalhes();
-
-        double creditosDaIniciativa = iniciativa.calculaCreditosGerados();
-
-        totalDeCreditos += creditosDaIniciativa;
+        iniciativas.add(iniciativa);
     }
 
-    public double getTotalDeCreditos() {
-        return totalDeCreditos;
+    public double calculaTotalDeCreditos() {
+        double creditosCalculados = 0.0;
+
+        for (IniciativaSustentavel i : iniciativas) {
+            creditosCalculados += i.calculaCreditosGerados();
+        }
+
+        return creditosCalculados;
+    }
+
+    public void geraRelatorioGeral() {
+        System.out.println("\n=======================================");
+        System.out.println("    RELATÓRIO DE IMPACTO AMBIENTAL   ");
+
+        for (IniciativaSustentavel i : iniciativas) {
+            i.exibeDetalhes();
+        }
+
+        System.out.println(">>> TOTAL DE CRÉDITOS DA EMPRESA: " + calculaTotalDeCreditos());
+        System.out.println("=======================================");
     }
 
 }
